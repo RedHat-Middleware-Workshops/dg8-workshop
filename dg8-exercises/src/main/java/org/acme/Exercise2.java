9704b26d5602110a8dfdca76e88bd9f0100c1219
sshaaf <sshaaf@redhat.com>
2020-04-13 18:24:44 +0200

    remove source covered by instruction and add exercise2

--- dg8-exercises/src/main/java/org/acme/Exercise2.java
+++ dg8-exercises/src/main/java/org/acme/Exercise2.java
@@ -10,19 +10,15 @@ import javax.cache.spi.CachingProvider;
 public class Exercise2 {
 
     public static void main(String[] args) {
-        // Construct a simple local cache manager with default configuration
-        CachingProvider jcacheProvider = Caching.getCachingProvider();
-        CacheManager cacheManager = jcacheProvider.getCacheManager();
-        MutableConfiguration<String, String> configuration = new MutableConfiguration<>();
-        configuration.setTypes(String.class, String.class);
-        // create a cache using the supplied configuration
-        Cache<String, String> cache = cacheManager.createCache("myCache", configuration);
-        // Store a value
-        cache.put("key", "value");
-        // Retrieve the value and print it out
-        System.out.printf("key = %s\n", cache.get("key"));
-        // Stop the cache manager and release all resources
-        cacheManager.close();
+        // TODO: Construct a simple local cache manager with default configuration
+
+        // TODO: create a cache using the supplied configuration
+
+        // Store and retrieve value
+
+        
+        // TODO: Stop the cache manager and release all resources
+
     }
 
 }
