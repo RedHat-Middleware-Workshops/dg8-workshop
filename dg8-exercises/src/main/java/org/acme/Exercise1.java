f9e42269142c9b93c249d22b4682dbcbfa4b16fe
sshaaf <sshaaf@redhat.com>
2020-04-13 17:41:13 +0200

    remove source covered by instruction and add exercise1

--- dg8-exercises/src/main/java/org/acme/Exercise1.java
+++ dg8-exercises/src/main/java/org/acme/Exercise1.java
@@ -8,18 +8,18 @@ import org.infinispan.manager.DefaultCacheManager;
 public class Exercise1 {
 
     public static void main(String[] args) {
-        // Construct a simple local cache manager with default configuration
-        DefaultCacheManager cacheManager = new DefaultCacheManager();
-        // Define local cache configuration
-        cacheManager.defineConfiguration("local", new ConfigurationBuilder().build());
-        // Obtain the local cache
-        Cache<String, String> cache = cacheManager.getCache("local");
-        // Store a value
-        cache.put("key", "value");
-        // Retrieve the value and print it out
-        System.out.printf("key = %s\n", cache.get("key"));
-        // Stop the cache manager and release all resources
-        cacheManager.stop();
+        // TODO: Construct a simple local cache manager with default configuration
+
+        // TODO: Define local cache configuration
+
+        // TODO: Obtain the local cache
+
+        // TODO: Store a value
+
+        // TODO: Retrieve the value and print it out
+
+        // TODO: Stop the cache manager and release all resources
+
     }
 
 }
