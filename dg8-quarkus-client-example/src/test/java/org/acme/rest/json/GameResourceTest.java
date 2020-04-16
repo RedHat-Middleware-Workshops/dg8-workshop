5d65fa976805eb8519aff6f3c343988a1e9640da
sshaaf <sshaaf@redhat.com>
2020-04-16 16:52:56 +0200

    add remotecache instructions

--- dg8-quarkus-client-example/src/test/java/org/acme/rest/json/GameResourceTest.java
+++ dg8-quarkus-client-example/src/test/java/org/acme/rest/json/GameResourceTest.java
@@ -13,39 +13,4 @@ import io.quarkus.test.junit.QuarkusTest;
 @QuarkusTest
 public class GameResourceTest {
 
-    @Test
-    public void testList() {
-        given()
-          .when().get("/fruits")
-          .then()
-             .statusCode(200)
-             .body("$.size()", is(2),
-                     "name", containsInAnyOrder("Apple", "Pineapple"),
-                     "description", containsInAnyOrder("Winter fruit", "Tropical fruit"));
-    }
-
-    @Test
-    public void testAdd() {
-        given()
-            .body("{\"name\": \"Pear\", \"description\": \"Winter fruit\"}")
-            .header("Content-Type", MediaType.APPLICATION_JSON)
-        .when()
-            .post("/fruits")
-        .then()
-            .statusCode(200)
-            .body("$.size()", is(3),
-                    "name", containsInAnyOrder("Apple", "Pineapple", "Pear"),
-                    "description", containsInAnyOrder("Winter fruit", "Tropical fruit", "Winter fruit"));
-
-        given()
-            .body("{\"name\": \"Pear\", \"description\": \"Winter fruit\"}")
-            .header("Content-Type", MediaType.APPLICATION_JSON)
-        .when()
-            .delete("/fruits")
-        .then()
-            .statusCode(200)
-            .body("$.size()", is(2),
-                    "name", containsInAnyOrder("Apple", "Pineapple"),
-                    "description", containsInAnyOrder("Winter fruit", "Tropical fruit"));
-    }
 }
