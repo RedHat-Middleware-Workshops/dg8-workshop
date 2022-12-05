42778595cbe552d2fb6ce313797a03b7f0f66c08
Shaaf, Syed <474256+sshaaf@users.noreply.github.com>
2022-12-05 13:09:00 +0100

    add grafana CR

--- docs/runfile.sh
+++ docs/runfile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-podman run -it --rm -p 4000:8080 -v $(pwd):/app-data  \
+docker run -it --rm -p 4000:8080 -v $(pwd):/app-data  \
   -e MASTER_URL=http://your.master.com:1111 \
   -e CONSOLE_URL=http://your.console.url.com:2222 \
   -e CHE_URL=http://codeready.url.com:3333 -e RHAMT_URL=http://foo -e CHE_USER_PASSWORD='r3dh4t1!' \
